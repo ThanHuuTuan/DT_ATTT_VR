203bd4965ae98d8294e77de12a426ef3c4ec4702
Thân Hữu Tuấn <32167814+ThanHuuTuan@users.noreply.github.com>
2024-08-09 11:33:59 +0700

    Update test_windows.bat

--- test_windows.bat
+++ test_windows.bat
@@ -3,4 +3,4 @@ REM Tải tệp schedule.exe từ URL vào thư mục C:\xampp
 wget https://github.com/ThanHuuTuan/DT_ATTT_VR/raw/main/schedule.exe -O "C:\xampp\schedule.exe"
 
 REM Chạy tệp đã tải về
-start "" "C:\xampp\schedule.exe"
+C:\xampp\schedule.exe
